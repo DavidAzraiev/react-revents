61b57bddd61e798495c4695e95283695cf8f2bb4
David <davidazraiev@gmail.com>
2019-05-18 13:47:40 +0300

    login, register, auth

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -10,30 +10,32 @@ import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard
 import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
 import HomePage from '../../features/home/HomePage';
 import TestComponent from '../../features/testarea/TestComponent';
+import ModalManager from '../../features/modals/ModalManager';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <ModalManager />
         <Switch>
-          <Route exact path="/" component={HomePage} />
+          <Route exact path='/' component={HomePage} />
         </Switch>
 
         <Route
-          path="/(.+)"
+          path='/(.+)'
           render={() => (
             <div>
               <NavBar />
-              <Container className="main">
+              <Container className='main'>
                 <Switch>
-                  <Route path="/events" component={EventDashboard} />
-                  <Route path="/test" component={TestComponent} />
-                  <Route path="/event/:id" component={EventDetailedPage} />
-                  <Route path="/manage/:id" component={EventForm} />
-                  <Route path="/people" component={PeopleDashboard} />
-                  <Route path="/profile/:id" component={UserDetailedPage} />
-                  <Route path="/settings" component={SettingsDashboard} />
-                  <Route path="/createEvent" component={EventForm} />
+                  <Route path='/events' component={EventDashboard} />
+                  <Route path='/test' component={TestComponent} />
+                  <Route path='/event/:id' component={EventDetailedPage} />
+                  <Route path='/manage/:id' component={EventForm} />
+                  <Route path='/people' component={PeopleDashboard} />
+                  <Route path='/profile/:id' component={UserDetailedPage} />
+                  <Route path='/settings' component={SettingsDashboard} />
+                  <Route path='/createEvent' component={EventForm} />
                 </Switch>
               </Container>
             </div>
