79c202bf8dc34693362d5def12aa769dad742c94
David <davidazraiev@gmail.com>
2019-05-25 17:24:11 +0300

    create new event to firestore

--- src/features/user/Settings/AboutPage.jsx
+++ src/features/user/Settings/AboutPage.jsx
@@ -8,12 +8,12 @@ import PlaceInput from '../../../app/common/form/PlaceInput';
 import SelectInput from '../../../app/common/form/SelectInput';
 
 const interests = [
-  { key: 'drinks', text: 'Drinks', value: 'drinks' },
-  { key: 'culture', text: 'Culture', value: 'culture' },
-  { key: 'film', text: 'Film', value: 'film' },
-  { key: 'food', text: 'Food', value: 'food' },
-  { key: 'music', text: 'Music', value: 'music' },
-  { key: 'travel', text: 'Travel', value: 'travel' }
+  { key: 'Drinks', text: 'Drinks', value: 'Drinks' },
+  { key: 'culture', text: 'Culture', value: 'Culture' },
+  { key: 'film', text: 'Film', value: 'Film' },
+  { key: 'food', text: 'Food', value: 'Food' },
+  { key: 'music', text: 'Music', value: 'Music' },
+  { key: 'travel', text: 'Travel', value: 'Travel' }
 ];
 
 const AboutPage = ({ pristine, submitting, handleSubmit, updateProfile }) => {
