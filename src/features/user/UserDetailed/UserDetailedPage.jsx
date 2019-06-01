40e5543c189e809d1131e26fe529b2cb55f52c10
David <davidazraiev@gmail.com>
2019-06-01 19:03:23 +0300

    users following and followers

--- src/features/user/UserDetailed/UserDetailedPage.jsx
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -10,7 +10,7 @@ import UserDetailedSidebar from './UserDetailedSidebar';
 import UserDetailedEvents from './UserDetailedEvents';
 import { userDetailedQuery } from '../userQueries';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
-import { getUserEvents } from '../userActions';
+import { getUserEvents, followUser, unfollowUser } from '../userActions';
 
 const mapState = (state, ownProps) => {
   let userUid = null;
@@ -32,12 +32,15 @@ const mapState = (state, ownProps) => {
     eventsLoading: state.async.loading,
     photos: state.firestore.ordered.photos,
     auth: state.firebase.auth,
-    requesting: state.firestore.status.requesting
+    requesting: state.firestore.status.requesting,
+    following: state.firestore.ordered.following
   };
 };
 
 const actions = {
-  getUserEvents
+  getUserEvents,
+  followUser,
+  unfollowUser
 };
 
 class UserDetailedPage extends Component {
@@ -58,16 +61,26 @@ class UserDetailedPage extends Component {
       match,
       requesting,
       events,
-      eventsLoading
+      eventsLoading,
+      followUser,
+      following,
+      unfollowUser
     } = this.props;
     const isCurrentUser = auth.uid === match.params.id;
     const loading = Object.values(requesting).some(a => a === true);
+    const isFollowing = !isEmpty(following);
     if (loading) return <LoadingComponent inverted={true} />;
     return (
       <Grid>
         <UserDetailedHeader profile={profile} />
         <UserDetailedDescription profile={profile} />
-        <UserDetailedSidebar isCurrentUser={isCurrentUser} />
+        <UserDetailedSidebar
+          isFollowing={isFollowing}
+          profile={profile}
+          followUser={followUser}
+          unfollowUser={unfollowUser}
+          isCurrentUser={isCurrentUser}
+        />
         {photos && photos.length > 0 && <UserDetailedPhotos photos={photos} />}
         <UserDetailedEvents
           events={events}
